74ce145700ddba67a458c2128dd1bfb927fd5a9e
Garrakx <garrakxmods@gmail.com>
2020-12-02 20:33:27 +0100

    Add customvariations (BLL/KELP)

---
+++ Custom-Regions/Creatures/DaddyLongLegsHook.cs
@@ -0,0 +1,40 @@
+﻿using CustomRegions.Mod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomRegions.Creatures
+{
+    static class DaddyLongLegsHook
+    {
+        public static void ApplyHooks()
+        {
+            On.DaddyLongLegs.ctor += DaddyLongLegs_ctor;
+        }
+
+        private static void DaddyLongLegs_ctor(On.DaddyLongLegs.orig_ctor orig, DaddyLongLegs self, AbstractCreature abstractCreature, World world)
+        {
+            orig(self, abstractCreature, world);
+            if (world != null)
+            {
+                //CustomWorldMod.Log($"Region Name [{self.region.name}]");
+                foreach (KeyValuePair<string, string> keyValues in CustomWorldMod.loadedRegions)
+                {
+                    //CustomWorldMod.Log($"Checking in [{CustomWorldMod.availableRegions[keyValues.Key].regionName}]");
+                    if (CustomWorldMod.availableRegions[keyValues.Key].regionConfig.TryGetValue(world.region.name, out CustomWorldStructs.RegionConfiguration config))
+                    {
+                        if (!config.bllVanilla)
+                        {
+                            CustomWorldMod.Log($"Spawning custom DDL/BLL in [{world.region.name}] from [{CustomWorldMod.availableRegions[keyValues.Key].regionName}]");
+                            self.colorClass = true;
+                            self.effectColor = config.bllColor ?? new UnityEngine.Color(0, 0, 1);
+                            self.eyeColor = self.effectColor;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Custom-Regions/Creatures/TentaclePlantGraphicsHook.cs
@@ -0,0 +1,63 @@
+﻿using CustomRegions.Mod;
+using RWCustom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace CustomRegions.Creatures
+{
+    static class TentaclePlantGraphicsHook
+    {
+        public static void ApplyHooks()
+        {
+            On.TentaclePlantGraphics.ctor += TentaclePlantGraphics_ctor; ;
+            On.TentaclePlantGraphics.ApplyPalette += TentaclePlantGraphics_ApplyPalette;
+        }
+
+        private static void TentaclePlantGraphics_ctor(On.TentaclePlantGraphics.orig_ctor orig, TentaclePlantGraphics self, PhysicalObject ow)
+        {
+            orig(self, ow);
+            customColor = null;
+            World world = ow.abstractPhysicalObject.world;
+            if (world != null)
+            {
+                //CustomWorldMod.Log($"Region Name [{self.region.name}]");
+                foreach (KeyValuePair<string, string> keyValues in CustomWorldMod.loadedRegions)
+                {
+                    //CustomWorldMod.Log($"Checking in [{CustomWorldMod.availableRegions[keyValues.Key].regionName}]");
+                    if (CustomWorldMod.availableRegions[keyValues.Key].regionConfig.TryGetValue(world.region.name, out CustomWorldStructs.RegionConfiguration config))
+                    {
+                        if (!config.kelpVanilla)
+                        {
+                            customColor = new Color[self.danglers.Length];
+                            
+                            CustomWorldMod.Log($"Spawning tentacle plant with custom color in [{world.region.name}] from [{CustomWorldMod.availableRegions[keyValues.Key].regionName}]");
+                            for (int i = 0; i < customColor.Length; i++)
+                            {
+                                HSLColor hsl = CRExtras.RGB2HSL(config.kelpColor ?? new Color());
+                                customColor[i] = Custom.HSL2RGB(Mathf.Lerp(hsl.hue*0.9f, hsl.hue * 1.1f, Mathf.Pow(self.danglerProps[i, 0], 1.6f)), Mathf.Lerp(hsl.saturation, 0.35f, self.danglerProps[i, 0]), Mathf.Lerp(hsl.lightness, 0.35f, self.danglerProps[i, 0]));
+                            }
+                        }
+                            break;
+                    }
+                }
+            }
+        }
+
+        private static Color[] customColor;
+
+        private static void TentaclePlantGraphics_ApplyPalette(On.TentaclePlantGraphics.orig_ApplyPalette orig, TentaclePlantGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette)
+        {
+            orig(self, sLeaser, rCam, palette);
+            if (customColor != null)
+            {
+                for (int i = 0; i < self.danglers.Length; i++)
+                {
+                    sLeaser.sprites[i + 1].color = Color.Lerp(customColor[i], palette.blackColor, rCam.room.Darkness(self.plant.rootPos));
+                }
+            }
+        }
+    }
+}
