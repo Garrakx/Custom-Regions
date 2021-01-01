648c23d4324e32e7d3d023c5c0225bb7f8a7c245
Garrakx <garrakxmods@gmail.com>
2021-01-01 18:02:09 +0100

    ModLoader checker

--- Custom-Regions/RainWorldHook.cs
+++ Custom-Regions/RainWorldHook.cs
@@ -1,8 +1,7 @@
 ﻿using CustomRegions.Mod;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Runtime.Remoting.Messaging;
 
 namespace CustomRegions
 {
@@ -15,18 +14,31 @@ namespace CustomRegions
 
         private static void RainWorld_Start(On.RainWorld.orig_Start orig, RainWorld self)
         {
+            //CustomWorldMod.Log($"Loaded assemblies... [{string.Join(", ", .ToArray())}]"); 
+            try
+            {
+                bool usingBepinex = false;
+                foreach (System.Reflection.Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    if (assembly.GetName().Name.Equals("BepInEx.MonoMod.Loader") || assembly.GetName().Name.Equals("BepInEx"))
+                    {
+                        usingBepinex = true;
+                        break;
+                    }
+                }
+                CustomWorldMod.usingBepinex = usingBepinex;
+                CustomWorldMod.Log($"Using BepInEx [{usingBepinex}]");
+            } 
+            catch (Exception e)
+            {
+                CustomWorldMod.Log("Error checking the modloaer " + e, true);
+            }
+
             CustomWorldMod.LoadCustomWorldResources();
-            CustomWorldMod.DownloadThumbs();
+            CustomWorldMod.rainWorldInstance = self;
 
             orig(self);
 
-            CustomWorldMod.rainWorldInstance = self;
-
-            if (ThumbnailDownloader.instance != null && ThumbnailDownloader.instance.readyToDelete)
-            {
-                ThumbnailDownloader.instance.Clear();
-                ThumbnailDownloader.instance = null;
-            }
         }
     }
 }
