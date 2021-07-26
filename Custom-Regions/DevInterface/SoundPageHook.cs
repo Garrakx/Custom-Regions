19b6b27f1fa7f5cedfdb340ed034ef0452621d1c
Garrakx <46248530+Garrakx@users.noreply.github.com>
2021-07-26 16:30:55 +0200

    Update SoundPageHook.cs

--- Custom-Regions/DevInterface/SoundPageHook.cs
+++ Custom-Regions/DevInterface/SoundPageHook.cs
@@ -21,8 +21,16 @@ namespace CustomRegions.DevInterface
             On.DevInterface.SoundPage.ctor -= SoundPage_ctor;
         }
 
-        private static void SoundPage_ctor(On.DevInterface.SoundPage.orig_ctor orig, global::DevInterface.SoundPage self, global::DevInterface.DevUI owner, string IDstring, global::DevInterface.DevUINode parentNode, string name)
+        private static void SoundPage_ctor(On.DevInterface.SoundPage.orig_ctor orig, SoundPage self, 
+            DevUI owner, string IDstring, DevUINode parentNode, string name)
         {
+            DirectoryInfo directoryInfo = new DirectoryInfo("./SoundEffects/Ambient/");
+            if (!directoryInfo.Exists)
+            {
+                Directory.CreateDirectory(directoryInfo.FullName);
+                CustomWorldMod.Log($"Creating directory at [{directoryInfo.FullName}] to avoid a crash.", true);
+            }
+
             try
             {
                 orig(self, owner, IDstring, parentNode, name);
@@ -33,55 +41,58 @@ namespace CustomRegions.DevInterface
             foreach (KeyValuePair<string, string> keyValues in CustomWorldMod.activatedPacks)
             {
                 string customPath = RWCustom.Custom.RootFolderDirectory() + CustomWorldMod.resourcePath + keyValues.Value + Path.DirectorySeparatorChar;
-                DirectoryInfo directoryInfo = new DirectoryInfo(customPath+"Assets/Futile/Resources/LoadedSoundEffects/Ambient/");
-                if (!directoryInfo.Exists)
+                directoryInfo = new DirectoryInfo(customPath + "Assets/Futile/Resources/LoadedSoundEffects/Ambient/");
+                if (directoryInfo.Exists)
                 {
-                    CustomWorldMod.Log($"{keyValues.Key} does not have loaded ambient sounds at {directoryInfo.FullName}", 
-                        false, CustomWorldMod.DebugLevel.FULL);
-                    continue;
-                }
-
-                FileInfo[] array = directoryInfo.GetFiles();
-                for (int i = 0; i < array.Length; i++)
-                {
-                    if (!list.Contains(array[i]))
+                    FileInfo[] array = directoryInfo.GetFiles();
+                    for (int i = 0; i < array.Length; i++)
                     {
-                        CustomWorldMod.Log($"Adding loaded ambient sounds {array[i]} from [{keyValues.Key}]", false, CustomWorldMod.DebugLevel.FULL);
-                        list.Add(array[i]);
-                    }
-                }
-                directoryInfo = new DirectoryInfo(customPath+"SoundEffects/Ambient/");
-                if (!directoryInfo.Exists)
-                {
-                    CustomWorldMod.Log($"{keyValues.Key} does not have ambient sounds at {directoryInfo.FullName}", false, CustomWorldMod.DebugLevel.FULL);
-                    continue;
-                }
-
-                array = directoryInfo.GetFiles();
-                for (int j = 0; j < array.Length; j++)
-                {
-                    bool flag = true;
-                    for (int k = 0; k < list.Count; k++)
-                    {
-                        if (list[k].Name == array[k].Name)
+                        if (!list.Contains(array[i]))
                         {
-                            flag = false;
-                            break;
+                            CustomWorldMod.Log($"Adding loaded ambient sounds {array[i]} from [{keyValues.Key}]", false, CustomWorldMod.DebugLevel.FULL);
+                            list.Add(array[i]);
                         }
                     }
-                    if (flag)
+                }
+                else
+                {
+                    CustomWorldMod.Log($"{keyValues.Key} does not have loaded ambient sounds at {directoryInfo.FullName}",
+                        false, CustomWorldMod.DebugLevel.FULL);
+                }
+
+                directoryInfo = new DirectoryInfo(customPath + "SoundEffects/Ambient/");
+                if (directoryInfo.Exists)
+                {
+                    FileInfo[] array = directoryInfo.GetFiles();
+                    for (int j = 0; j < array.Length; j++)
                     {
-                        CustomWorldMod.Log($"Adding ambient sounds {array[j]} from [{keyValues.Key}]", false, CustomWorldMod.DebugLevel.FULL);
-                        list.Add(array[j]);
+                        bool flag = true;
+                        for (int k = 0; k < list.Count; k++)
+                        {
+                            if (list[k].Name == array[k].Name)
+                            {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag)
+                        {
+                            CustomWorldMod.Log($"Adding ambient sounds {array[j]} from [{keyValues.Key}]", false, CustomWorldMod.DebugLevel.FULL);
+                            list.Add(array[j]);
+                        }
+                    }
+                    // CURSED
+                    for (int l = list.Count - 1; l >= 0; l--)
+                    {
+                        if (list[l].Name.Length > 5 && list[l].Name.Substring(list[l].Name.Length - 5, 5) == ".meta")
+                        {
+                            list.RemoveAt(l);
+                        }
                     }
                 }
-                // CURSED
-                for (int l = list.Count - 1; l >= 0; l--)
+                else
                 {
-                    if (list[l].Name.Length > 5 && list[l].Name.Substring(list[l].Name.Length - 5, 5) == ".meta")
-                    {
-                        list.RemoveAt(l);
-                    }
+                    CustomWorldMod.Log($"{keyValues.Key} does not have ambient sounds at {directoryInfo.FullName}", false, CustomWorldMod.DebugLevel.FULL);
                 }
 
             }
@@ -93,7 +104,8 @@ namespace CustomRegions.DevInterface
             }
             self.files = currentFiles.ToArray();
 
-            CustomWorldMod.Log($"Loaded ambient sounds: [{string.Join(", ", self.files.Select(l => l.Name).ToArray())}]");
+            CustomWorldMod.Log($"Loaded ambient sounds: [{string.Join(", ", self.files.Select(l => l.Name).ToArray())}]", 
+                false, CustomWorldMod.DebugLevel.FULL);
 
 			self.totalFilePages = 1 + (int)((float)self.files.Length / (float)self.maxFilesPerPage + 0.5f);
 			self.RefreshFilesPage();
