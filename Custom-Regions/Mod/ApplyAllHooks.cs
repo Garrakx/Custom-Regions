03f2f83dbe501941b1ed94d9165af054a6242c18
Garrakx <46248530+Garrakx@users.noreply.github.com>
2021-07-26 15:46:46 +0200

    Reorder hooks, add "RemoveHooks"

--- Custom-Regions/Mod/ApplyAllHooks.cs
+++ Custom-Regions/Mod/ApplyAllHooks.cs
@@ -17,72 +17,127 @@ namespace CustomRegions.Mod
     {
         public static void ApplyAllHooks()
         {
-            MapHook.ApplyHook();
-            RegionGateHook.ApplyHooks();
-            RegionHook.ApplyHook();
-            RoomSettingsHook.ApplyHook();
-            WorldHook.ApplyHook();
-            WorldLoaderHook.ApplyHooks();
-            OverWorldHook.ApplyHooks();
-            PlayerProgressionHook.ApplyHooks();
+            // Ordered by folders in alphabetic
 
-            // Pearl
-            DataPearlHook.ApplyHooks();
-            SLOracleBehaviorHasMarkHook.ApplyHooks();
-            PlacedObjectHook.ApplyHooks();
 
-            // Rain world instance
-            RainWorldHook.ApplyHooks();
+            /// ARENAS ///
+            ArenaCreatureSpawnerHook.ApplyHooks();
+            MultiplayerUnlocksHook.ApplyHooks();
 
-            // Custom Palette
-            RoomCameraHook.ApplyHook();
+            /// CREATURES ///
+            BigEelHook.ApplyHooks();
+            DaddyLongLegsHook.ApplyHooks();
+            LizardGraphicsHook.ApplyHooks();
+            TentaclePlantGraphicsHook.ApplyHooks();
 
-            // Electric gate
-            RoomHook.ApplyHooks();
-            WaterGateHook.ApplyHooks();
-
-            // Custom Decal
-            //CustomDecalHook.ApplyHook();
-
-            MainLoopProcessHook.ApplyHooks();
-
-            // Scene
+            /// CUSTOM MENU ///
             FastTravelScreenHook.ApplyHooks();
-            //MainMenuHook.ApplyHooks();
-            MenuSceneHook.ApplyHook();
-            MenuIllustrationHook.ApplyHook();
+            MenuIllustrationHook.ApplyHooks();
+            MenuSceneHook.ApplyHooks();
+            MultiplayerMenuHook.ApplyHooks();
             SlugcatSelectMenuHook.ApplyHooks();
 
-            // DevInterface
+            /// CUSTOM PEARLS ///
+            DataPearlHook.ApplyHooks();
+            PlacedObjectHook.ApplyHooks();
+            SLOracleBehaviorHasMarkHook.ApplyHooks();
+
+            /// DEV INTERFACE ///
             MapPageHook.ApplyHooks();
             MapRenderOutputHook.ApplyHooks();
             CustomDecalRepresentationHook.ApplyHooks();
+            SoundPageHook.ApplyHooks();
 
-            // Arena
-            MultiplayerMenuHook.ApplyHook();
-            ArenaCreatureSpawnerHook.ApplyHook();
+            /// HUD ///
+            MapHook.ApplyHooks();
 
-            // WinState - achievement
-            WinStateHook.ApplyHook();
-
-            SaveStateHook.ApplyHook();
-
-            // MusicPiece
+            /// MUSIC ///
             MusicPieceHook.ApplyHooks();
             ProceduralMusicInstructionsHook.ApplyHooks();
 
-            BigEelHook.ApplyHooks();
-            TentaclePlantGraphicsHook.ApplyHooks();
-            DaddyLongLegsHook.ApplyHooks();
-            LizardGraphicsHook.ApplyHooks();
 
+            /// WORLD ///
+            OverWorldHook.ApplyHooks();
+            RegionGateHook.ApplyHooks();
+            RegionHook.ApplyHooks();
+            WorldHook.ApplyHooks();
+            WorldLoaderHook.ApplyHooks();
+
+
+            MainLoopProcessHook.ApplyHooks();
+            PlayerProgressionHook.ApplyHooks();
+            // Rain world instance
+            RainWorldHook.ApplyHooks();
+            // Custom Palette
+            RoomCameraHook.ApplyHooks();
+            RoomHook.ApplyHooks();
+            RoomSettingsHook.ApplyHooks();
+            SaveStateHook.ApplyHooks();
+            // WinState - achievement
+            WinStateHook.ApplyHooks();
             // WWW
             WWWHook.ApplyHooks();
+        }
 
-            RegionStateHook.ApplyHooks();
+        public static void RemoveAllHooks()
+        {
+            /// ARENAS ///
+            ArenaCreatureSpawnerHook.RemoveHooks();
+            MultiplayerUnlocksHook.RemoveHooks();
 
-            // Unlocks ID
-            MultiplayerUnlocksHook.ApplyHooks();
+            /// CREATURES ///
+            BigEelHook.RemoveHooks();
+            DaddyLongLegsHook.RemoveHooks();
+            LizardGraphicsHook.RemoveHooks();
+            TentaclePlantGraphicsHook.RemoveHooks();
+
+            /// CUSTOM MENU ///
+            FastTravelScreenHook.RemoveHooks();
+            MenuIllustrationHook.RemoveHooks();
+            MenuSceneHook.RemoveHooks();
+            MultiplayerMenuHook.RemoveHooks();
+            SlugcatSelectMenuHook.RemoveHooks();
+
+            /// CUSTOM PEARLS ///
+            DataPearlHook.RemoveHooks();
+            PlacedObjectHook.RemoveHooks();
+            SLOracleBehaviorHasMarkHook.RemoveHooks();
+
+            /// DEV INTERFACE ///
+            MapPageHook.RemoveHooks();
+            MapRenderOutputHook.RemoveHooks();
+            CustomDecalRepresentationHook.RemoveHooks();
+            SoundPageHook.RemoveHooks();
+
+            /// HUD ///
+            MapHook.RemoveHooks();
+
+            /// MUSIC ///
+            MusicPieceHook.RemoveHooks();
+            ProceduralMusicInstructionsHook.RemoveHooks();
+
+
+            /// WORLD ///
+            OverWorldHook.RemoveHooks();
+            RegionGateHook.RemoveHooks();
+            RegionHook.RemoveHooks();
+            WorldHook.RemoveHooks();
+            WorldLoaderHook.RemoveHooks();
+
+
+            MainLoopProcessHook.RemoveHooks();
+            PlayerProgressionHook.RemoveHooks();
+            // Rain world instance
+            RainWorldHook.RemoveHooks();
+            // Custom Palette
+            RoomCameraHook.RemoveHooks();
+            RoomHook.RemoveHooks();
+            RoomSettingsHook.RemoveHooks();
+            SaveStateHook.RemoveHooks();
+            // WinState - achievement
+            WinStateHook.RemoveHooks();
+            // WWW
+            WWWHook.RemoveHooks();
         }
     }
 }
