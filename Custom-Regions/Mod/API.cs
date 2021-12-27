d90d5d506c37f5fac10e6ed6344695dd33637ff5
Garrakx <46248530+Garrakx@users.noreply.github.com>
2021-12-27 10:45:02 +0100

    Fix API, add comments

--- Custom-Regions/Mod/API.cs
+++ Custom-Regions/Mod/API.cs
@@ -26,9 +26,42 @@ namespace CustomRegions.Mod
 
         public delegate void RegionPreprocessor(RegionInfo info);
 
+        /// <summary>
+        /// Add region pre processor to the list.
+        /// </summary>
+        /// <param name="pre"> Method which filters or modifies API.RegionInfo</param>
+        public static void AddRegionPreprocessor(RegionPreprocessor pre)
+        {
+            CustomWorldMod.Log($"Adding preprocessor! [{pre.Method.Name}]");
+            if (CustomWorldMod.regionPreprocessors == null)
+            {
+                CustomWorldMod.Log("Initiation region preprocessors from other mod...");
+                CustomWorldMod.regionPreprocessors = new List<RegionPreprocessor>();
+            }
+            if (CustomWorldMod.regionPreprocessors.Contains(pre))
+            {
+                CustomWorldMod.Log($"Adding duplicated preprocessor! [{pre.Method.Name}]", true);
+                return;
+            }
+            CustomWorldMod.regionPreprocessors.Add(pre);
+        }
+
+        /// <summary>
+        /// Dictionary with all installed region packs, where the Key is the region pack name and the value is a struct with its information.
+        /// </summary>
         public static Dictionary<string, CustomWorldStructs.RegionPack> InstalledPacks { get => CustomWorldMod.installedPacks; }
+
+        /// <summary>
+        /// Dictionary with activated region packs, where the Key is the region pack name and the value is the folder.
+        /// </summary>
         public static Dictionary<string, string> ActivatedPacks { get => CustomWorldMod.activatedPacks; }
+
         public static List<CustomWorldStructs.PackDependency> InstalledDependencies { get => CustomWorldMod.installedDependencies; }
+
+        /// <summary>
+        /// List containing unused dependencies, no region pack is using them so they can be disabled.
+        /// </summary>
+        public static List<CustomWorldStructs.PackDependency> UnusedDependencies { get => CustomWorldMod.unusedDependencies; }
 
         /// <summary>
         /// Indicates wheter CRS is currently downloading a pack or not.
@@ -56,5 +89,24 @@ namespace CustomRegions.Mod
             CustomWorldMod.LoadCustomWorldResources();
         }
 
+        /// <summary>
+        /// Builds a folder path. It will return a specific file if specified, otherwise it will end with backslash. Can also be used to navigate vanilla folders.
+        /// </summary>
+        /// <param name="regionPackFolder"> Folder name of the region pack. Use null for vanilla path. </param>
+        /// <param name="folderName"> Folder desired to build the path to (with spaces or underscore)</param>
+        /// <param name="regionID"> Region ID needed for Rooms or RegionID folder.</param>
+        /// <param name="file"> If specified, it will append a file after the folder path.</param>
+        /// <param name="folder"> If specified, it will append an additional folder path.</param>
+        /// <returns>Path built.</returns>
+        public static string BuildPath(string regionPackFolder, string folderName, string regionID = null,
+            string file = null, string folder = null, bool includeRoot = true)
+        {
+            CRExtras.CustomFolder folderEnum;
+            string folderNameEdited = folderName.Replace(" ", "_");
+            try { folderEnum = (CRExtras.CustomFolder)Enum.Parse(typeof(CRExtras.CustomFolder), folderNameEdited); }
+            catch ( Exception e) { CustomWorldMod.Log($"Folder [{folderName}] not supported by CRS, did you make a typo? {e}", true); return null; }
+
+            return CRExtras.BuildPath(regionPackFolder, folderEnum, regionID, file, folder, includeRoot);
+        }
     }
 }
