fb9d83bd014fa7e3422b02c896e64e089562ee73
Bro748 <103339131+Bro748@users.noreply.github.com>
2023-09-28 14:05:09 -0600

    Better Convo Support
    Allows Saint pearls to exist, and also better support for other mods to use custom convo

--- src/Collectables/CustomConvo.cs
+++ src/Collectables/CustomConvo.cs
@@ -10,13 +10,35 @@ using Random = UnityEngine.Random;
 
 namespace CustomRegions.Collectables
 {
-    internal static class CustomConvo
+    public static class CustomConvo
     {
-        public static void ApplyHooks()
+        internal static void ApplyHooks()
         {
             On.Conversation.DataPearlToConversation += Conversation_DataPearlToConversation;
             On.SLOracleBehaviorHasMark.MoonConversation.AddEvents += MoonConversation_AddEvents;
             IL.Conversation.LoadEventsFromFile_int_Name_bool_int += Conversation_LoadEventsFromFileIL;
+            IL.SLOracleBehaviorHasMark.GrabObject += SLOracleBehaviorHasMark_GrabObject;
+        }
+
+        /// <summary>
+        /// Lets Saint pearls be read if Saint-specific txts exist
+        /// </summary>
+        private static void SLOracleBehaviorHasMark_GrabObject(ILContext il)
+        {
+            var c = new ILCursor(il);
+            if (c.TryGotoNext(MoveType.After,
+                x => x.MatchLdsfld<DataPearl.AbstractDataPearl.DataPearlType>(nameof(DataPearl.AbstractDataPearl.DataPearlType.LF_west)),
+                x => x.MatchCall(out _)))
+            {
+                c.Emit(OpCodes.Ldarg_0);
+                c.Emit(OpCodes.Ldarg_1);
+                c.EmitDelegate((bool flag, SLOracleBehaviorHasMark self, PhysicalObject item) => {
+                    string pearl = (item as DataPearl).abstractPhysicalObject.type.value;
+                    SlugcatStats.Name name = self.oracle.room.game.StoryCharacter;
+
+                    return flag || SearchConvoFile(self, pearl, name, out _, slugExclusive: true) != null; 
+                });
+            }
         }
 
         private static void Conversation_LoadEventsFromFileIL(MonoMod.Cil.ILContext il)
@@ -34,49 +56,14 @@ namespace CustomRegions.Collectables
                 c.Emit(OpCodes.Ldarg_2);
                 c.Emit(OpCodes.Ldloc_0);
                 c.Emit(OpCodes.Ldloc_1);
-                c.EmitDelegate((Conversation self, int fileName, SlugcatStats.Name saveFile, InGameTranslator.LanguageID languageID, string original) => SearchConvoFile(self, fileName.ToString(), saveFile, languageID) ?? original);
+                c.EmitDelegate((Conversation self, int fileName, SlugcatStats.Name saveFile, InGameTranslator.LanguageID languageID, string original) => SearchConvoFile(self.interfaceOwner, fileName.ToString(), saveFile, out _) ?? original);
                 c.Emit(OpCodes.Stloc_1);
             }
-            else 
+            else
             {
                 CustomRegionsMod.BepLogError("failed to il hook Conversation.LoadEventsFromFile!");
             }
         }
-
-        public static string SearchConvoFile(Conversation self, string fileName, SlugcatStats.Name saveFile, InGameTranslator.LanguageID languageID)
-        {
-            string langDirectory = self.interfaceOwner.rainWorld.inGameTranslator.SpecificTextFolderDirectory(languageID) + Path.DirectorySeparatorChar;
-
-            string oracleName = "";
-
-            if (self.interfaceOwner is OracleBehavior behavior)
-            {
-                oracleName = behavior.oracle.ID.value + Path.DirectorySeparatorChar;
-            }
-
-            string slugName = "";
-            if (saveFile != null)
-            {
-                slugName = "-" + saveFile.value;
-            }
-
-            for (int i = 0; i < 4; i++)
-            {
-                string text = i switch
-                {
-                    0 => langDirectory + oracleName + fileName + slugName + ".txt",
-                    1 => langDirectory + oracleName + fileName + ".txt",
-                    2 => langDirectory + fileName + slugName + ".txt",
-                    _ => langDirectory + fileName + ".txt"
-                };
-                CustomRegionsMod.CustomLog($"Searching for pearl convo at path [{text}]: {(File.Exists(AssetManager.ResolveFilePath(text)) ? "Found!" : "Not Found")}", false, CustomRegionsMod.DebugLevel.FULL);
-                
-                if (File.Exists(AssetManager.ResolveFilePath(text))) return AssetManager.ResolveFilePath(text);
-            }
-            return null;
-        }
-
-        static string Check(string path) => File.Exists(AssetManager.ResolveFilePath(path)) ? AssetManager.ResolveFilePath(path) : null;
 
         private static void MoonConversation_AddEvents(On.SLOracleBehaviorHasMark.MoonConversation.orig_AddEvents orig, SLOracleBehaviorHasMark.MoonConversation self)
         {
@@ -106,36 +93,70 @@ namespace CustomRegions.Collectables
 
         public static void LoadEventsFromFile(Conversation self, string fileName, SlugcatStats.Name saveFile = null, bool oneRandomLine = false, int randomSeed = 0)
         {
+            LoadEventsFromFile(self, fileName, null, saveFile, oneRandomLine, randomSeed);
+        }
+
+        public static void LoadEventsFromFile(Conversation self, string fileName, Oracle.OracleID oracleID, SlugcatStats.Name saveFile = null, bool oneRandomLine = false, int randomSeed = 0)
+        {
             if (saveFile == null) { saveFile = self.currentSaveFile; }
 
             CustomRegionsMod.CustomLog("~~~LOAD CONVO " + fileName);
-            InGameTranslator.LanguageID languageID = self.interfaceOwner.rainWorld.inGameTranslator.currentLanguage;
-            string realName;
-            for (; ; )
-            {
-                realName = CustomConvo.SearchConvoFile(self, fileName, saveFile, languageID);
-                if (File.Exists(realName))
-                {
-                    goto IL_117;
-                }
-                CustomRegionsMod.CustomLog("NOT FOUND " + realName);
-                if (languageID == InGameTranslator.LanguageID.English)
-                {
-                    break;
-                }
-                CustomRegionsMod.CustomLog("RETRY WITH ENGLISH");
-                languageID = InGameTranslator.LanguageID.English;
-            }
-            return;
-        IL_117:
-            string fileText = Encryption.DecryptCustomText(realName, languageID, fileName);
+
+            string path = CustomConvo.SearchConvoFile(self.interfaceOwner, fileName, saveFile, out var languageID, oracleID);
+            if (!File.Exists(path)) return;
+
+            string fileText = Encryption.DecryptCustomText(path, languageID, fileName);
 
             CustomRegionsMod.CustomLog(fileText, false, CustomRegionsMod.DebugLevel.FULL);
 
             string[] array = Regex.Split(fileText, "\r\n");
+            ParseConvoText(self, array, oneRandomLine, randomSeed);
+        }
+
+        public static string SearchConvoFile(Conversation.IOwnAConversation interfaceOwner, string fileName, SlugcatStats.Name saveFile, out InGameTranslator.LanguageID languageID, Oracle.OracleID oracleID = null, bool iteratorExclusive = false, bool slugExclusive = false)
+        {
+            languageID = interfaceOwner.rainWorld.inGameTranslator.currentLanguage;
+
+            string oracleName = "";
+            if (oracleID != null) oracleName = oracleID.value + Path.DirectorySeparatorChar;
+            else if (interfaceOwner is OracleBehavior behavior) oracleName = behavior.oracle.ID.value + Path.DirectorySeparatorChar;
+
+            string slugName = saveFile != null ? "-" + saveFile.value : "";
+
+            for (; ; )
+            {
+                string langDirectory = interfaceOwner.rainWorld.inGameTranslator.SpecificTextFolderDirectory(languageID) + Path.DirectorySeparatorChar;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    string text = i switch
+                    {
+                        0 => langDirectory + oracleName + fileName + slugName + ".txt",
+                        1 => langDirectory + oracleName + fileName + ".txt",
+                        2 => langDirectory + fileName + slugName + ".txt",
+                        _ => langDirectory + fileName + ".txt"
+                    };
+                    CustomRegionsMod.CustomLog($"Searching for pearl convo at path [{text}]: {(File.Exists(AssetManager.ResolveFilePath(text)) ? "Found!" : "Not Found")}", false, CustomRegionsMod.DebugLevel.FULL);
+
+                    if (iteratorExclusive && (i != 0 || i != 1)) continue;
+                    if (slugExclusive && (i != 0 || i != 2)) continue;
+
+                    if (File.Exists(AssetManager.ResolveFilePath(text))) return AssetManager.ResolveFilePath(text);
+                }
+
+                CustomRegionsMod.CustomLog("NOT FOUND " + fileName);
+                if (languageID == InGameTranslator.LanguageID.English) break;
+
+                CustomRegionsMod.CustomLog("RETRY WITH ENGLISH");
+                languageID = InGameTranslator.LanguageID.English;
+            }
+            return null;
+        }
+
+        public static void ParseConvoText(Conversation self, string[] array, bool oneRandomLine = false, int randomSeed = 0)
+        {
             try
             {
-
                 if (oneRandomLine)
                 {
                     List<Conversation.TextEvent> list = new List<Conversation.TextEvent>();
@@ -201,5 +222,6 @@ namespace CustomRegions.Collectables
                 self.events.Add(new Conversation.TextEvent(self, 0, "TEXT ERROR", 100));
             }
         }
+
     }
 }
